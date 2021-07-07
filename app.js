f5dd74e3c12e459fcb9b689ec0a177aca06fedcd
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-07 11:59:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-const express = require('express')
+const express = require('express') // include express
+const mongoose = require('mongoose') // include mongoose
 const app = express()
+
+// connect to mongoose
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// get connect status
+const db = mongoose.connection
+
+// connect error
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// connect success
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   app.send('hello world!')
