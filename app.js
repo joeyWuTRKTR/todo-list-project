69c1d369cf8977eda6a12c9b54f2fea2d4e43099
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-08 20:22:26 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express') // include express
 const mongoose = require('mongoose') // include mongoose
 const exphbs = require('express-handlebars')
 const Todo = require('./models/todo')
+const bodyParser = require('body-parser')
 const app = express()
 
 // connect to mongoose
@@ -23,11 +24,25 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Todo.find() // find data from database
     .lean()  // transfer data into js
     .then(todos => res.render( 'index', { todos })) // render template
     .catch(error => console.log(error)) // show the error
+})
+
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name  // take "name" from req.body
+  const todo = new Todo({ name })
+  return todo.save() // store in database
+    .then(() => res.redirect('/')) // finish creation, lead to index
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
