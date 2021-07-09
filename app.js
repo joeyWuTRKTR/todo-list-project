a90d07b9d7189358cfdac28421d56eab92961d08
joeyWuTRKTR <godatascial@yahoo.com>
2021-07-09 10:02:04 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -76,6 +76,15 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// Delete
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove()) // remove the data
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 app.listen(3000, () => {
   console.log('app is listening on port 3000!')
 })
